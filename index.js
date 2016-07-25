95401fb7425e5664f287fa610b477ba12fa40376
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-07-25 16:57:30 -0400

    Test commit

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 var stylelint = require("stylelint");
 
-var ruleName = "tmi/max-width-query";
-
-stylelint.utils.ruleMessages(ruleName, {
-  expected: "Expected ...",
+var ruleName = "tmi/max-width-query"
+var messages = stylelint.utils.ruleMessages(ruleName, {
+  rejected: function(classname) { return "NO!" }
 })
 
-module.exports = stylelint.createPlugin(ruleName, function (expectation) {
-  const runColorHexCase = stylelint.rules["color-hex-case"](expectation)
-  return (root, result) => {
-    if (root.toString().indexOf("@@check-color-hex-case") === -1) return
-    runColorHexCase(root, result)
+module.exports = stylelint.createPlugin(ruleName, function(enabled) {
+  return function(root, result) {
+
+    var validOptions = stylelint.utils.validateOptions(result, ruleName, {
+      actual: enabled,
+      possible: [true, false]
+    })
+
+    if (!validOptions) { return }
+
+    var utilityClasses = primerUtilities.cssstats.selectors.values
+
+    if (utilityClasses.length == 0) {
+      return
+    }
+
+    root.walkRules(function(rule) {
+
+      var ruleClasses = rule.selector.match(/\.[a-z\-_0-9]+/ig)
+
+      if (ruleClasses != null) {
+        for (var i = 0; i < ruleClasses.length; i++) {
+          var ruleClass = ruleClasses[i]
+          if (utilityClasses.indexOf(ruleClass) >= 0) {
+            stylelint.utils.report({
+              message: messages.rejected(ruleClass),
+              node: rule,
+              result: result,
+              ruleName: ruleName
+            })
+          }
+        }
+      }
+    })
   }
 })
+
+module.exports.ruleName = ruleName
+module.exports.messages = messages
