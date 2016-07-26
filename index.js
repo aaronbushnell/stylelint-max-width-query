e7f775e47a85d8ff67d3d48bfe6cd9a1f87e535a
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-07-25 23:18:36 -0400

    Finalize code

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ var stylelint = require("stylelint");
 var styleSearch = require('style-search');
 
 var ruleName = "tmi/max-width-query"
-var messages = stylelint.utils.ruleMessages(ruleName, {
-  rejected: function() { return "asdfasdf" }
-})
 
-module.exports = stylelint.createPlugin(ruleName, function(enabled) {
-  return function(root, result) {
-
+module.exports = stylelint.createPlugin(ruleName, function (enabled) {
+  return function (root, result) {
     var validOptions = stylelint.utils.validateOptions(result, ruleName, {
       actual: enabled,
       possible: [true, false]
@@ -16,14 +12,20 @@ module.exports = stylelint.createPlugin(ruleName, function(enabled) {
 
     if (!validOptions) { return }
 
-    styleSearch({
-      source: result,
-      target: "green",
-    }, function(match, count) {
-      console.log('taco!');
-    });
+    root.walkAtRules(function (statement) {
+      styleSearch({
+        source: statement.toString(),
+        target: "@media (max-width",
+      }, function (match, count) {
+        stylelint.utils.report({
+          ruleName: ruleName,
+          result: result,
+          node: statement,
+          message: 'Expected min-width instead of max-width query.'
+        });
+      });
+    })
   }
 })
 
 module.exports.ruleName = ruleName
-module.exports.messages = messages
